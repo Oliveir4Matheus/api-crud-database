f054bb2e694a500f7121f94bc83c1fbc71e716da
Oliveir4Matheus <contato.matheusoliv@outlook.com>
2024-11-22 16:32:22 -0300

    FEAT: add connection with database

--- server.js
+++ server.js
@@ -1,8 +1,27 @@
-import express from 'express';
+import express from 'express'
+import { PrismaClient } from '@prisma/client'
 
-const app = express();
+const prisma = new PrismaClient()
+
+const app = express()
+app.use(express.json())
+
+const users = []
+app.post('/users',async(req,res) => {
+    await prisma.User.create({
+        data:{
+            name: req.body.name,
+            age: req.body.age,
+            email: req.body.email,
+            posts: req.body.posts
+        }
+    })
+    res.status(201).json(req.body)
+})
 
 app.get('/users',(req,res) => {
-    res.send('ok deu bom');
-});
-app.listen(3000);
+    res.status(200).json(users)
+
+    res.send('ok deu bom')
+})
+app.listen(3000)
